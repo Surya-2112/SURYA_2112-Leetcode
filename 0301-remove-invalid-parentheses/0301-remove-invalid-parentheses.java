ee5882df2b0c23b771cb5f3e91177bec58a1d58c
Surya-2112 <717822s157@kce.ac.in>
2025-07-31 11:50:32 +0530

    Time: 376 ms (8.36%), Space: 45.3 MB (54.81%) - LeetHub

--- 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.java
+++ 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.java
@@ -1,26 +1,17 @@
 class Solution {
     int max=0;
     public static Set<String> l;
-    public boolean isVaild(String str)
-    {
-        int count=0;
-        for(char c:str.toCharArray())
-        {
-           if(c=='(') count++;
-           if(c==')') count--;
-           if(count<0) return false;
-        }
-        return count==0;
-    } 
-    public void pickNotPick(char arr[],int ind ,int  n,String str)
-    {  StringBuilder s=new StringBuilder(str);
+    public void pickNotPick(char arr[],int ind ,int  n,String str,int open)
+    {   if(open<0) return ;
+         StringBuilder s=new StringBuilder(str);
+       if(str.length()+(n-ind)<max) return ;
         while(ind<n &&arr[ind]!='(' && arr[ind]!=')')
         {
             s.append(arr[ind++]);
         }
         if(ind==n)
         {   str=s.toString();
-             if(isVaild(str))
+             if(open==0)
              {   if(max<str.length())
                 {   max=str.length();
                     l=new HashSet<String>();
@@ -30,17 +21,22 @@ class Solution {
              }
             return ;
         }
+        if(arr[ind]=='(') open++;
+        else open--;
         s.append(arr[ind]);
-        pickNotPick(arr,ind+1,n,s.toString());
-        s.deleteCharAt(s.length()-1);
-        pickNotPick(arr,ind+1,n,s.toString());
+        pickNotPick(arr,ind+1,n,s.toString(),open);
+        s.setLength(s.length()-1);
+         if(arr[ind]=='(') open--;
+        else open++;
+        pickNotPick(arr,ind+1,n,s.toString(),open);
     }
     public List<String> removeInvalidParentheses(String s) {
         l=new HashSet<>();
+        l.add("");
         int max=0;
         char arr[]=s.toCharArray();
         int n=arr.length;
-        pickNotPick(arr,0,n,"");
+        pickNotPick(arr,0,n,"",0);
         return new ArrayList<String>(l);
     }
 }
